451e17e18b79db0c1932b7fc794c87490280bbcc
Maulik Garach <69416773+garachmaulik@users.noreply.github.com>
2020-09-08 13:11:40 +0530

    Add files via upload

---
+++ Deck.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Deck{
+private final ArrayList<Card> deckCards;
+
+public Deck(){this.deckCards = initDeck();}
+
+private ArrayList<Card> initDeck(){
+final ArrayList<Card> deckCards = new ArrayList<>();
+for(final Card.Suit suit: Card.Suit.values()){
+for(final Card.Rank rank: Card.Rank.values()){
+deckCards.add(Card.getCard(rank,suit));
+}
+}
+return deckCards;
+}
+
+public ArrayList<Card> getDeck(){return deckCards;}
+
+public ArrayList<Card> shuffle(){
+Collections.shuffle(deckCards);
+return deckCards;}
+
+public ArrayList<Card> sort(){
+Collections.sort(deckCards);
+return deckCards;}
+
+} // End of Deck
